376a48c4d2208523a2cc7524ac92d2d6a3f870d2
Yile Cai <78542909+Yile99@users.noreply.github.com>
2022-12-29 00:34:54 +1100

    224

---
+++ 224 计算机 ？？？？？？？？？？？？？？？？？？？？.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int calculate(String s) {
+        Stack<Integer> stack= new Stack<Integer>();
+        int sign =1;
+        int res = 0;
+        for (int i = 0 ; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(Character.isDigit(ch)){
+                int value = ch - '0';
+            while(i+1 < s.length() && Character.isDigit(s.charAt(i+1))){
+                i++;
+                value = value*10+s.charAt(i)-'0';
+            }
+            res = res+sign*value;
+            }else if(ch=='+'){
+                sign = 1;
+            }else if(ch =='-'){
+                sign = -1;
+            }else if (ch == '('){
+                stack.push(res);
+                res = 0;
+                stack.push(sign);
+                sign = 1;
+            }else if(ch ==')'){
+                int formerSign = stack.peek();
+                stack.pop();
+                int formerRes = stack.peek();
+                stack.pop();
+                res = formerRes +formerSign * res;
+            }
+        }
+        return res;
+    }
+}
