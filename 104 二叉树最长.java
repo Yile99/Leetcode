263a3a9e552bba68cf8b6c9ce6619681eae9a891
Yile Cai <78542909+Yile99@users.noreply.github.com>
2023-01-19 01:45:25 +1100

    104

---
+++ 104 二叉树最长.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        int MaxDepth = Math.max(left,right);
+        return MaxDepth +1;
+    }
+}
