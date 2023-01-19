f044f398c352eca39304fbe65d9e98f6b64f3300
Yile Cai <78542909+Yile99@users.noreply.github.com>
2023-01-20 00:52:01 +1100

    50

---
+++ 50算x次方.java
@@ -0,0 +1,14 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if(n == 0){
+            return 1.0;
+        }else if(n % 2 == 0){
+            x = myPow(x * x, n / 2);
+        }else if(n > 0){
+            x = myPow(x * x, n / 2) * x;
+        }else if(n < 0){
+            x = myPow(x * x, n / 2) * (1.0 / x);
+        }
+        return x;
+    }
+}
